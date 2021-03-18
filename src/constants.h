b94e3014901d10505cf3826db59e5fdc31a43a38
Amaury Vanoorenberghe <amaury.vanoorenberghe@yahoo.com>
2021-03-18 18:20:15 +0100

    Created constants header file

---
+++ src/constants.h
@@ -0,0 +1,17 @@
+/*
+— ’ ’ représente les cases vides
+— ’#’ représente les murs (cases infranchissables)
+— ’$’ représente une caisse que vous pouvez déplacer
+— ’.’ représente une zone de rangement sur laquelle vous devez placer une caisse
+— ’*’ représente une zone de rangement sur laquelle une caisse est actuellement placée
+— ’@’ représente votre personnage
+— ’+’ représente votre personnage lorsqu’il est sur une zone de rangement.
+*/
+
+#define TILE_EMPTY ' '
+#define TILE_WALL '#'
+#define TILE_CRATE '$'
+#define TILE_TARGET '.'
+#define TILE_CRATE_ON_TARGET '*'
+#define TILE_PLAYER '@'
+#define TILE_PLAYER_ON_TARGET '+'
