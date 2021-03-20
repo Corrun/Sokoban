7f4cec684d0e543455ae408bbd8c719006777854
Néo ALMEIDA <neo.almeida.etu@univ-lille.fr>
2021-03-20 18:59:45 +0100

    Added a files contains all types of the project

--- include/main.h
+++ include/main.h
@@ -1,19 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
-  int lignes;
-  int colonnes;
-}point_t;
-
-typedef struct{
-  int lignes; // Nombre de lignes du niveau (hauteur en cases)
-  int colonnes; // Nombre de colonnes du niveau (largeur en cases)
-  char* terrain; // Contenu du niveau sous forme de caractères
-  point_t* perso;
-}niveau_t;
-
-
+#include "./types.h"
 #include "./constants.h"
 #include "./point_t.h"
 #include "./niveau_t.h"
