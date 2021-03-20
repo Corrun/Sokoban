941811cbbad0ff4ac8dcf4f59de2ce21e079670c
Néo ALMEIDA <neo.almeida.etu@univ-lille.fr>
2021-03-20 16:15:54 +0100

    Types are now def in main.h | 2 new fonctions (need to be test) in pointt.c | E2 Q2 is maybe finish

--- include/main.h
+++ include/main.h
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+  int lignes;
+  int colonnes;
+}point_t;
+
+typedef struct{
+  int lignes; // Nombre de lignes du niveau (hauteur en cases)
+  int colonnes; // Nombre de colonnes du niveau (largeur en cases)
+  char* terrain; // Contenu du niveau sous forme de caractères
+  point_t* perso;
+}niveau_t;
+
+
 #include "./constants.h"
 #include "./point_t.h"
 #include "./niveau_t.h"
-
--- include/point_t.h
+++ include/point_t.h
@@ -1,6 +1,5 @@
-typedef struct{
-  int lignes;
-  int colonnes;
-}point_t;
+
 
 char entree_du_joueur(void);
+point_t* nouveau_personnage(int colonne, int ligne);
+point_t* trouver_perso(niveau_t* niveau);
