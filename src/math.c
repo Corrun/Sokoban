102bbb5a97bc1b747ad7ff734abe305db197b990
Amaury Vanoorenberghe <amaury.vanoorenberghe@yahoo.com>
2021-03-21 23:14:59 +0100

    Added comments to math.c

--- src/math.c
+++ src/math.c
@@ -1,6 +1,8 @@
 #include "main.h"
 
+// Renvoie la valeur minimale entrée en paramètre
 int min (int premier, int deuxieme){
+//Compare les deux paramètres, et permet de renvoyer le plus petit
 	if(premier > deuxieme){
 		return deuxieme;
 	}else{
@@ -8,7 +10,9 @@ int min (int premier, int deuxieme){
 	}
 }
 
+// Renvoie la valeur maximale entrée en paramètre
 int max (int premier, int deuxieme){
+	//Compare les deux paramètres, et permet de renvoyer le plus grand
 	if(premier < deuxieme){
 		return deuxieme;
 	}else{
