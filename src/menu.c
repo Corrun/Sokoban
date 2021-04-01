ec3f28ed76a283e2787c4bcc3a0f26d2decfc1c0
Neo ALMEIDA <almeidan@bouleau14.iut-infobio.priv.univ-lille1.fr>
2021-04-01 09:20:51 +0200

    Added comments

--- src/menu.c
+++ src/menu.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
+// Affiche le niveau de manière (très) brut.
 void afficher_menu(){
    printf("\n                                      ▄▄                           \n ▄█▀▀▀█▄█        ▀███                ▄██                           \n▄██    ▀█          ██                 ██                           \n▀███▄     ▄██▀██▄  ██  ▄██▀  ▄██▀██▄  ██▄████▄  ▄█▀██▄ ▀████████▄  \n  ▀█████▄██▀   ▀██ ██ ▄█    ██▀   ▀██ ██    ▀████   ██   ██    ██  \n▄     ▀████     ██ ██▄██    ██     ██ ██     ██ ▄█████   ██    ██  \n██     ████▄   ▄██ ██ ▀██▄  ██▄   ▄██ ██▄   ▄████   ██   ██    ██  \n█▀█████▀  ▀█████▀▄████▄ ██▄▄ ▀█████▀  █▀█████▀ ▀████▀██▄████  ████▄\n"); 
    sleep(2);
 }
 
+// Permet de demander à l'utilisateur un int, pour ensuite choisir le niveau
 int choix_du_niveau(){
     int choix;
     printf("Choisis un niveau ! : ");
