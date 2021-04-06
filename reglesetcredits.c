dd08fdec4aaab77ed48dde02f515b60a8025485a
Amaury Vanoorenberghe <amaury.vanoorenberghe@yahoo.com>
2021-04-06 18:00:25 +0200

    TEMP File for the rules and the credits
    clear-authored-by: Ninhache <mr.carambar27@gmail.com>

---
+++ reglesetcredits.c
@@ -0,0 +1,19 @@
+printf("Règles classiques de Sokoban :\n
+Le joueur doit placer toutes les caisses (%c) sur toutes les cibles (%c)\n
+Mais attention !\n
+Un joueur est trop faible pour pousser 2 caisses à la fois ...\n
+Ou même les tirer !\n
+\n
+Pour retourner au menu, appuyer sur Q.", DISPLAY_TILE_CRATE, DISPLAY_TILE_TARGET);
+
+
+printf("Un grand merci et bravo aux contributeurs du projets\n
+Je cite :\n
+\tALMEIDA Néo\n
+\tROUX Hugo\n
+\tVANOORENBERGHE Amaury\n
+\n
+Et aux librairies natives\n
+Et à la superbe librairie Ncurses !\n
+\n
+Pour retourner au menu, appuyer sur Q.");
